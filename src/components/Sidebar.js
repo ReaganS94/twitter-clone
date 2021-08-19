62d666dc18bbcb62766b4a770e2f22ee14c82e06
Reagan Sasan <reagansasani@hotmail.com>
2021-08-19 14:04:18 +0200

    first commit pt2

---
+++ src/components/Sidebar.js
@@ -0,0 +1,31 @@
+import HomeIcon from "@material-ui/icons/Home";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+
+import SidebarOption from "./SidebarOption";
+
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <TwitterIcon />
+      <SidebarOption Icon={HomeIcon} text="Home" />
+      <SidebarOption Icon={SearchIcon} text="Explore" />
+      <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
+      <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+      <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+      <SidebarOption Icon={ListAltIcon} text="Lists" />
+      <SidebarOption Icon={PersonOutlineIcon} text="Profile" />
+      <SidebarOption Icon={MoreHorizIcon} text="More" />
+    </div>
+  );
+}
+
+export default Sidebar;
