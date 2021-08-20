33b03dfb891efe25b9ca0cc53a721945936e9aee
Reagan Sasan <reagansasani@hotmail.com>
2021-08-20 11:04:50 +0200

    searchQuery

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Sidebar from "./components/Sidebar";
@@ -7,11 +9,13 @@ import Widgets from "./components/Widgets";
 import "./App.css";
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   return (
     <div className="App">
       <Sidebar />
-      <Feed />
-      <Widgets />
+      <Feed searchQuery={searchQuery} />
+      <Widgets setSearchQuery={setSearchQuery} />
     </div>
   );
 }
